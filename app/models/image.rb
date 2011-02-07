0faafb3448f96cec33f73d3edc88e5579047c7c0
Ondross <andrew.heine@students.olin.edu>
2011-02-07 11:07:59 -0500

    I have no idea what I changed, shit

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,3 @@
 class Image < ActiveRecord::Base
-has_attached_file :photo,
-      :storage => :s3,
-      :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-      :path => "/:style/:filename"
+has_attached_file :photo
 end
