ee9c8092ca86662e68d792ee40df8fbaae409368
Ondross <andrew.heine@students.olin.edu>
2011-02-07 12:25:50 -0500

    amazon one step at a time, searching for error

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,5 +3,5 @@ has_attached_file :photo,
       :storage => :s3,
       :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
       :path => "/:filename"
-	  :url => "http://www.funnyforumpics.com/forums/LOL/1/File0004205.JPG"
+	  #:url => "http://www.funnyforumpics.com/forums/LOL/1/File0004205.JPG"
 end
