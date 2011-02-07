f0bee60d96bf42109331c137b104e97004cbf2fc
Ondross <andrew.heine@students.olin.edu>
2011-02-07 12:02:04 -0500

    amazon one step at a time, searching for error

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,4 +3,5 @@ has_attached_file :photo,
       :storage => :s3,
       :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
       :path => "/:filename"
+	  :url => "/:class/:attachment/:id/:basename.:extension"
 end
